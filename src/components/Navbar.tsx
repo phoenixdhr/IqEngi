84dd18f45da4aaa8fed5943a7612e49a40aa06e9
dreyes <phoenixdhr@gmail.com>
2024-02-16 23:45:53 -0500

    menu funcional

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ import {
   SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon,  CalendarIcon, NewspaperIcon, BriefcaseIcon} from '@heroicons/react/20/solid'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon,  CalendarIcon, NewspaperIcon, BriefcaseIcon, ArrowRightStartOnRectangleIcon} from '@heroicons/react/20/solid'
 
 // Datos de ejemplo para productos y acciones principales
 const products = [
@@ -49,8 +49,25 @@ export default function Example() {
           </a>
         </div>
 
+
+
         {/* Botón para mostrar el menú en dispositivos móviles */}
         <div className="flex lg:hidden">
+
+          {/* Botón Crear Cuenta */}
+          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-4 ">Crear Cuenta</button>
+
+          {/* Simbolo de flecha de iniciar sesión */}
+          <div className=" flex items-center justify-between me-3">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                <ArrowRightStartOnRectangleIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                </a>
+          </div>
+
+          {/* Botón Sandwich*/}
           <button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -60,6 +77,8 @@ export default function Example() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
+
+
 
 
 
@@ -138,10 +157,16 @@ export default function Example() {
 
 
         {/* Enlace para iniciar sesión, visible solo en pantallas grandes */}
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center">
+
+          <div className="flex">
+          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ">Crear Cuenta</button>
+          </div>
+
           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
+          Iniciar Sesión <span aria-hidden="true">&rarr;</span>
           </a>
+
         </div>
 
 
@@ -231,14 +256,17 @@ export default function Example() {
 
               </div>
 
+
               {/* Enlace para iniciar sesión en el menú móvil */}
-              <div className="py-6">
+              <div className="py-6 flex items-center justify-between">
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Log in
+                  Iniciar Sesión
                 </a>
+              {/* Simbolo de flecha de iniciar sesión */}
+                <ArrowRightStartOnRectangleIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
               </div>
 
             </div>
